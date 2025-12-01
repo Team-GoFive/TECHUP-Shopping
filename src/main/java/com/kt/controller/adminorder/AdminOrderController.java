8a46f5429dff5fca0c7232cb3ed9bd2170f32051
김도현 <142032659+l-lyun@users.noreply.github.com>
2025-12-01 14:20:45 +0900

    [FEAT] AdminOrderController 구현 (#108)
    * feat: AdminOrderController 구현
    
    * feat: AdminOrderUpdateTest 구현
    
    * feat: AdminOrderListTest 구현
    
    * feat: OrderEntityCreator 오타 수정
    
    * feat: api 엔드포인트 및 put -> patch
    
    * feat: 테스트파일 엔드포인트 맞게 변경
    
    * feat: 주문 취소, 주문 상세 조회 테스트
    
    * feat: ErrorException 적용 후 테스트 확인

---
+++ src/main/java/com/kt/controller/adminorder/AdminOrderController.java
@@ -0,0 +1,64 @@
+package com.kt.controller.adminorder;
+
+import static com.kt.common.api.ApiResult.*;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kt.common.Paging;
+import com.kt.common.api.ApiResult;
+import com.kt.common.api.PageResponse;
+import com.kt.constant.OrderStatus;
+import com.kt.domain.dto.response.AdminOrderResponse;
+import com.kt.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin/orders")
+public class AdminOrderController {
+	public final OrderService orderService;
+
+	@GetMapping
+	public ResponseEntity<ApiResult<PageResponse<AdminOrderResponse.Search>>> searchOrder(
+		@ModelAttribute Paging paging
+	) {
+		return page(orderService.searchOrder(
+			paging.toPageable()
+		));
+	}
+
+	@GetMapping("/{orderId}")
+	public ResponseEntity<ApiResult<AdminOrderResponse.Detail>> getOrderDetail(
+		@PathVariable UUID orderId
+	) {
+		return wrap(orderService.getOrderDetail(orderId));
+	}
+
+	@PatchMapping("/{orderId}/change-status")
+	public ResponseEntity<ApiResult<Void>> updateOrderStatus(
+		@PathVariable UUID orderId,
+		@RequestBody OrderStatus newStatus
+	) {
+		orderService.updateOrderStatus(orderId, newStatus);
+		return empty();
+	}
+
+	@PatchMapping("/{orderId}/cancel")
+	public ResponseEntity<ApiResult<Void>> cancelOrder(
+		@PathVariable UUID orderId
+	) {
+		orderService.cancelOrder(orderId);
+		return empty();
+	}
+}
---
+++ src/test/java/com/kt/api/admin/order/OrderDetailTest.java
@@ -0,0 +1,81 @@
+package com.kt.api.admin.order;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+
+import com.kt.common.MockMvcTest;
+import com.kt.common.OrderEntityCreator;
+import com.kt.common.UserEntityCreator;
+import com.kt.constant.UserRole;
+import com.kt.domain.entity.OrderEntity;
+import com.kt.domain.entity.UserEntity;
+import com.kt.repository.OrderRepository;
+import com.kt.repository.user.UserRepository;
+import com.kt.security.DefaultCurrentUser;
+
+@DisplayName("주문 상세 조회(어드민) - get api/orders/{orderId}")
+public class OrderDetailTest extends MockMvcTest {
+
+	OrderEntity savedOrder;
+	UserEntity savedUser;
+	DefaultCurrentUser userDetails = new DefaultCurrentUser(
+		UUID.randomUUID(),
+		"test@example.com",
+		UserRole.ADMIN
+	);
+
+	@Autowired
+	private OrderRepository orderRepository;
+	@Autowired
+	private UserRepository userRepository;
+
+	@AfterEach
+	public void tearDown() {
+		orderRepository.deleteAll();
+	}
+
+	@Test
+	void 주문_상세_조회_성공_200() throws Exception {
+		// given
+		UserEntity user = UserEntityCreator.createMember();
+
+		savedUser = userRepository.save(user);
+		OrderEntity order = OrderEntityCreator.createOrderEntity(savedUser);
+		savedOrder = orderRepository.save(order);
+
+		mockMvc.perform(get("/api/admin/orders/{orderId}", order.getId())
+				.with(SecurityMockMvcRequestPostProcessors.user(userDetails))
+				.contentType(MediaType.APPLICATION_JSON)
+			).
+			andDo(print())
+			.andExpectAll(status().isOk())
+			.andExpect(jsonPath("$.code").value("ok"))
+			.andExpect(jsonPath("$.message").value("성공"));
+	}
+
+	// TODO: 에러 익셉션 적용 후 수정
+	@Test
+	void 주문_상세_조회__실패_404_NotFound() throws Exception {
+		// given
+
+		UUID randomOrderId = UUID.randomUUID();
+		mockMvc.perform(get("/api/orders/{orderId}", randomOrderId)
+				.with(SecurityMockMvcRequestPostProcessors.user(userDetails))
+				.contentType(MediaType.APPLICATION_JSON)
+			).
+			andDo(print())
+			.andExpectAll(status().isNotFound());
+	}
+
+}
