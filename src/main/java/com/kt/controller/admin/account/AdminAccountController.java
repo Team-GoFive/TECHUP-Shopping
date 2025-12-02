89ad48739938dc8c5dafd7b96b673ff7658d85f5
junsang-ee <dkwnstkddk@gmail.com>
2025-12-02 15:34:49 +0900

    fix:   - uri conflict 이슈 해결

--- src/main/java/com/kt/controller/admin/account/AdminAccountController.java
+++ src/main/java/com/kt/controller/admin/account/AdminAccountController.java
@@ -8,14 +8,11 @@ import com.kt.domain.dto.request.AccountRequest;
 
 import com.kt.domain.dto.response.AccountResponse;
 
-import com.kt.domain.dto.response.UserResponse;
-
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,23 +43,6 @@ public class AdminAccountController {
 				paging.toPageable()
 			)
 		);
-	}
-
-	@GetMapping("/users/{userId}")
-	public ResponseEntity<ApiResult<UserResponse.UserDetail>> getAccountDetail(@PathVariable UUID userId) {
-		return wrap(userService.getUserDetail(userId));
-	}
-
-	@PatchMapping("/users/{userId}/enabled")
-	public ResponseEntity<ApiResult<Void>> enableAccount(@PathVariable UUID userId) {
-		userService.enableUser(userId);
-		return empty();
-	}
-
-	@PatchMapping("/users/{userId}/disabled")
-	public ResponseEntity<ApiResult<Void>>  disableAccount(@PathVariable UUID userId) {
-		userService.disableUser(userId);
-		return empty();
 	}
 
 	@DeleteMapping("/accounts/{accountId}")
