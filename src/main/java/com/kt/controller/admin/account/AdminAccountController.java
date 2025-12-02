88f6d4419f1e52ce84b41f4b833e308bbf7dfee0
junsang <89866144+junsang-ee@users.noreply.github.com>
2025-12-02 15:45:02 +0900

    [BUG | etc] API 엔드포인트 중복 이슈 해결 (#131)
    * fix:
      - uri conflict 이슈 해결 (쓰이지 않는 의존성 제거)

--- src/main/java/com/kt/controller/admin/account/AdminAccountController.java
+++ src/main/java/com/kt/controller/admin/account/AdminAccountController.java
@@ -8,21 +8,17 @@ import com.kt.domain.dto.request.AccountRequest;
 
 import com.kt.domain.dto.response.AccountResponse;
 
-import com.kt.domain.dto.response.UserResponse;
-
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kt.common.Paging;
 import com.kt.service.AccountService;
-import com.kt.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,7 +28,6 @@ import static com.kt.common.api.ApiResult.*;
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminAccountController {
-	private final UserService userService;
 	private final AccountService accountService;
 
 	@GetMapping("/accounts")
@@ -46,23 +41,6 @@ public class AdminAccountController {
 				paging.toPageable()
 			)
 		);
-	}
-
-	@GetMapping("/users/{userId}")
-	public ResponseEntity<ApiResult<UserResponse.UserDetail>> getAccountDetail(@PathVariable UUID userId) {
-		return wrap(userService.getUserDetail(userId));
-	}
-
-	@PatchMapping("/users/{userId}/enabled")
-	public ResponseEntity<ApiResult<Void>> enableAccount(@PathVariable UUID userId) {
-		userService.enableUser(userId);
-		return empty();
-	}
-
-	@PatchMapping("/users/{userId}/disabled")
-	public ResponseEntity<ApiResult<Void>>  disableAccount(@PathVariable UUID userId) {
-		userService.disableUser(userId);
-		return empty();
 	}
 
 	@DeleteMapping("/accounts/{accountId}")
