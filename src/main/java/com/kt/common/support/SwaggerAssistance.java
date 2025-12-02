0da05c517a4fbf7fdf14b015509159b588a522da
l-lyun <kimdohyun032@gmail.com>
2025-12-02 17:37:14 +0900

    fix: SwaggerAssistance add 200

--- src/main/java/com/kt/common/support/SwaggerAssistance.java
+++ src/main/java/com/kt/common/support/SwaggerAssistance.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @ApiResponses(value = {
+	@ApiResponse(responseCode = "200", description = "요청 성공"),
 	@ApiResponse(responseCode = "400", description = "잘못된 요청입니다."),
 	@ApiResponse(responseCode = "500", description = "서버에러 - 관리자에게 문의하세요.")
 })
