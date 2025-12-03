7c48d4c48f08d8ab289ba50edc2b13430fc04e4b
terry <tryterry77@gmail.com>
2025-12-03 16:19:01 +0900

    refactor: AuthServiceTest 리팩토링 및 구현 코드 변경

---
+++ src/test/java/com/kt/common/SignupCourierRequestCreator.java
@@ -0,0 +1,27 @@
+package com.kt.common;
+
+import static java.util.UUID.*;
+
+import com.kt.constant.Gender;
+import com.kt.domain.dto.request.SignupRequest;
+
+public class SignupCourierRequestCreator {
+
+	public static SignupRequest.SignupCourier createSignupCourierRequest() {
+		return new SignupRequest.SignupCourier(
+			"테스트기사",
+			randomUUID() + "@test.com",
+			"1231231!",
+			Gender.MALE
+		);
+	}
+
+	public static SignupRequest.SignupCourier createSignupCourierRequest(String email) {
+		return new SignupRequest.SignupCourier(
+			"테스트기사",
+			email,
+			"1231231!",
+			Gender.MALE
+		);
+	}
+}
