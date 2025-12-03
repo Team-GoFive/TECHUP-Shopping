fbe0ca5cf765bbdf7fd0a67330ecddb4e4922854
kf16 <wjdtn747@naver.com>
2025-12-03 22:20:14 +0900

    feat: courier 검증 로직 구현

--- src/main/java/com/kt/controller/courier/CourierController.java
+++ src/main/java/com/kt/controller/courier/CourierController.java
@@ -3,6 +3,7 @@ package com.kt.controller.courier;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kt.common.api.ApiResult;
 import com.kt.domain.dto.request.CourierRequest;
 import com.kt.domain.dto.response.CourierResponse;
+import com.kt.security.DefaultCurrentUser;
 import com.kt.service.CourierService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,20 +28,22 @@ public class CourierController implements CourierSwaggerSupporter {
 	@Override
 	@GetMapping("/{courierId}")
 	public ResponseEntity<ApiResult<CourierResponse.Detail>> getCourierDetail(
+		@AuthenticationPrincipal DefaultCurrentUser defaultCurrentUser,
 		@PathVariable UUID courierId
 	){
 		return ApiResult.wrap(
-			courierService.getDetail(courierId)
+			courierService.getDetail(defaultCurrentUser.getId(), courierId)
 		);
 	}
 
 	@Override
 	@PutMapping("/{courierId}")
 	public ResponseEntity<ApiResult<Void>> updateCourier(
+		@AuthenticationPrincipal DefaultCurrentUser defaultCurrentUser,
 		@PathVariable UUID courierId,
 		@RequestBody CourierRequest.UpdateDetails request
 	){
-		courierService.updateDetail(courierId, request);
+		courierService.updateDetail(defaultCurrentUser.getId(), courierId, request);
 		return ApiResult.empty();
 	}
 }
