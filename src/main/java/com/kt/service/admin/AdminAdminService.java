e3a0d32323136744f20433cfcd019700bd7b9c1e
l-lyun <kimdohyun032@gmail.com>
2025-12-12 16:14:40 +0900

    refactor: User Admin-User 분리

---
+++ src/main/java/com/kt/service/admin/AdminAdminService.java
@@ -0,0 +1,22 @@
+package com.kt.service.admin;
+
+import java.util.UUID;
+
+import com.kt.domain.dto.request.SignupRequest;
+import com.kt.domain.dto.request.UserRequest;
+import com.kt.domain.dto.response.UserResponse;
+
+public interface AdminAdminService {
+	void createAdmin(UUID userId, SignupRequest.SignupMember request);
+
+	void deleteAdmin(UUID currentId, UUID adminId);
+
+	UserResponse.UserDetail getAdminDetail(UUID currentId, UUID subjectId);
+
+	void updateDetail(
+		UUID currentId,
+		UUID subjectId,
+		UserRequest.UpdateDetails details
+	);
+
+}
