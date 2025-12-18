a5894b2cb39bfdd537c1f286a1ef9c2cd133ca8c
l-lyun <kimdohyun032@gmail.com>
2025-12-18 21:25:13 +0900

    feat: 판매자 주문 관련 테스트 작성
    # Conflicts:
    #	src/test/java/com/kt/common/CurrentUserCreator.java

---
+++ src/test/java/com/kt/api/seller/order/OrderCancelTest.java
@@ -0,0 +1,103 @@
+package com.kt.api.seller.order;
+
+import com.kt.common.CurrentUserCreator;
+import com.kt.common.MockMvcTest;
+import com.kt.common.SellerEntityCreator;
+import com.kt.common.UserEntityCreator;
+import com.kt.constant.OrderProductStatus;
+import com.kt.domain.entity.CategoryEntity;
+import com.kt.domain.entity.OrderEntity;
+import com.kt.domain.entity.OrderProductEntity;
+import com.kt.domain.entity.ProductEntity;
+import com.kt.domain.entity.ReceiverVO;
+import com.kt.domain.entity.SellerEntity;
+import com.kt.domain.entity.UserEntity;
+import com.kt.repository.CategoryRepository;
+import com.kt.repository.order.OrderRepository;
+import com.kt.repository.orderproduct.OrderProductRepository;
+import com.kt.repository.product.ProductRepository;
+import com.kt.repository.seller.SellerRepository;
+import com.kt.repository.user.UserRepository;
+import com.kt.security.DefaultCurrentUser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static com.kt.common.CategoryEntityCreator.createCategory;
+import static com.kt.common.ProductEntityCreator.createProduct;
+import static com.kt.common.SellerEntityCreator.createSeller;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("판매자 주문 상품 취소 - PATCH /api/seller/orders/{orderProductId}/cancel")
+public class OrderCancelTest extends MockMvcTest {
+
+	@Autowired
+	private OrderProductRepository orderProductRepository;
+	@Autowired
+	private ProductRepository productRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
+	@Autowired
+	private SellerRepository sellerRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private OrderRepository orderRepository;
+
+	private SellerEntity testSeller;
+	private UserEntity testUser;
+	private CategoryEntity testCategory;
+	private OrderEntity testOrder;
+	private OrderProductEntity testOrderProduct;
+	private DefaultCurrentUser sellerDetails;
+
+	@BeforeEach
+	void setUp() {
+		testUser = UserEntityCreator.createMember();
+		userRepository.save(testUser);
+
+		testSeller = SellerEntityCreator.createSeller();
+		sellerRepository.save(testSeller);
+
+		testCategory = createCategory();
+		categoryRepository.save(testCategory);
+
+		sellerDetails = CurrentUserCreator.getSellerUserDetails(testSeller.getId());
+
+		ReceiverVO receiverVO = ReceiverVO.create("test", "010-1234-5678", "city", "district", "road", "detail");
+		testOrder = orderRepository.save(OrderEntity.create(receiverVO, testUser));
+
+		ProductEntity product = createProduct(testCategory, testSeller);
+		productRepository.save(product);
+		testOrderProduct = orderProductRepository.save(
+			OrderProductEntity.create(1L, product.getPrice(), OrderProductStatus.CREATED, testOrder, product));
+	}
+
+	@Test
+	void 판매자_주문_상품_취소_성공__200_OK() throws Exception {
+		// given
+		testOrderProduct.updateStatus(OrderProductStatus.SHIPPING_READY);
+		orderProductRepository.save(testOrderProduct);
+
+		// when
+		ResultActions actions = mockMvc.perform(
+				patch("/api/seller/orders/{orderProductId}/cancel", testOrderProduct.getId())
+					.with(user(sellerDetails)))
+			.andDo(print());
+
+		// then
+		actions.andExpect(status().isOk())
+			.andExpect(jsonPath("$.code").value("ok"));
+
+		OrderProductEntity cancelledOrderProduct = orderProductRepository.findById(testOrderProduct.getId()).orElseThrow();
+		assertThat(cancelledOrderProduct.getStatus()).isEqualTo(OrderProductStatus.CANCELED);
+	}
+}
