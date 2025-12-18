e2d10c5d000d4a288730939b25c24045f6b9219a
l-lyun <kimdohyun032@gmail.com>
2025-12-18 21:23:15 +0900

    feat: 판매자 주문 조회 querydsl 적용

--- src/main/java/com/kt/constant/OrderProductStatus.java
+++ src/main/java/com/kt/constant/OrderProductStatus.java
@@ -5,15 +5,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum OrderProductStatus {
 	CREATED("주문생성"),
-	PURCHASE_CONFIRMED("구매확정"),
-	CANCELED("주문취소"),
+	PAID("결제완료"),
 	SHIPPING_READY("배송대기"),
 	SHIPPING("배송중"),
 	SHIPPING_COMPLETED("배송완료"),
+	PURCHASE_CONFIRMED("구매확정"),
+	CANCELED("주문취소"),
 	RETURN_WAITING("반품대기"),
 	RETURN_SHIPPING("반품배송중"),
 	RETURN_ARRIVAL("반품도착"),
-	RETURN_CONFIRMED("반품확정");
+	RETURN_CONFIRMED("반품확정"),
+	;
 
 	private final String description;
 }
