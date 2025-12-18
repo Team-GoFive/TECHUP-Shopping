3dd667a81dea9e90b789a4e51ecbbbda795da76b
junsang-ee <dkwnstkddk@gmail.com>
2025-12-18 23:27:13 +0900

    feat:   - 관리자 엔티티 도메인 (AdminEntity) 불변 및 단일 객체 보장 확보 (unique key (adminCode) 추가)

--- src/main/java/com/kt/domain/entity/AdminEntity.java
+++ src/main/java/com/kt/domain/entity/AdminEntity.java
@@ -6,18 +6,35 @@ import com.kt.constant.Gender;
 
 import com.kt.constant.UserStatus;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
-@Entity(name = "admin")
+@Entity
+@Table(
+	name = "admin",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			name = "uk_admin_code",
+			columnNames = "admin_code"
+		)
+	}
+)
 @DiscriminatorValue("ADMIN")
 @NoArgsConstructor(access = PROTECTED)
 public class AdminEntity extends AbstractAccountEntity {
+
+	public static final String SYSTEM_ADMIN_CODE = "SYSTEM_ADMIN";
+
+	@Column(name = "admin_code", nullable = false, updatable = false)
+	private String adminCode;
 
 	protected AdminEntity(
 		String name,
@@ -31,6 +48,7 @@ public class AdminEntity extends AbstractAccountEntity {
 		this.gender = gender;
 		this.role = AccountRole.ADMIN;
 		this.status = UserStatus.ENABLED;
+		this.adminCode = SYSTEM_ADMIN_CODE;
 	}
 
 	public static AdminEntity create(
