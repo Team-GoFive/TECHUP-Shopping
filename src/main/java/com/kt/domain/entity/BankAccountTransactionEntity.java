1a271c6ff721aa0aa32616aba8df6e2b19580fb4
junsang-ee <dkwnstkddk@gmail.com>
2025-12-19 18:15:42 +0900

    refactor: 계좌거래내역엔티티 1. 거래 금액 및 거래 후 잔액 추가 2. @Table 제거 및 @Entity(name = "{name}") 으로 변경 3. BankAccount 연관관계 정의

--- src/main/java/com/kt/domain/entity/BankAccountTransactionEntity.java
+++ src/main/java/com/kt/domain/entity/BankAccountTransactionEntity.java
@@ -6,21 +6,39 @@ import com.kt.domain.entity.common.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
-@Entity
-@Table(name = "bank_account_transactions")
+@Entity(name = "bank_account_transactions")
 @NoArgsConstructor(access = PROTECTED)
 public class BankAccountTransactionEntity extends BaseEntity {
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "bank_account_id", nullable = false)
+	private BankAccountEntity bankAccount;
+
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private BankAccountTransactionType transactionType;
 
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private BankAccountTransactionPurpose transactionPurpose;
+
+	@Column(precision = 19, scale = 0, nullable = false)
+	private BigDecimal amount;
+
+	@Column(precision = 19, scale = 0, nullable = false)
+	private BigDecimal balanceSnapshot;
 }
