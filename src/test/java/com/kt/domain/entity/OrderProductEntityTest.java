6607f34d31c513c2a9d7b81e63ca291dfe989232
minseo <114375668+oneokiwa@users.noreply.github.com>
2025-12-23 01:46:18 +0900

    feat: OrderProductEntity 도메인 테스트 코드 구현

---
+++ src/test/java/com/kt/domain/entity/OrderProductEntityTest.java
@@ -0,0 +1,91 @@
+package com.kt.domain.entity;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.kt.constant.AccountRole;
+import com.kt.constant.Gender;
+import com.kt.constant.OrderProductStatus;
+
+@ActiveProfiles("test")
+class OrderProductEntityTest {
+	@Test
+	void 객체생성_성공() {
+
+		// given
+		UserEntity member = UserEntity.create(
+			"주문자테스터1",
+			"wjd123@naver.com",
+			"1234",
+			AccountRole.MEMBER,
+			Gender.MALE,
+			LocalDate.of(1990, 1, 1),
+			"010-1234-5678"
+		);
+
+		OrderEntity order = OrderEntity.create(
+			ReceiverVO.create(
+				"수령인",
+				"010-1234-5678",
+				"부산광역시",
+				"상세주소",
+				"101호",
+				"주소설명"
+			),
+			member
+		);
+
+		CategoryEntity category = CategoryEntity.create(
+			"테스트카테고리",
+			null
+		);
+
+		SellerEntity seller = SellerEntity.create(
+			"판매자1",
+			"seller@test.com",
+			"1234",
+			"상점1",
+			"010-1234-5678",
+			"seller@test.com",
+			Gender.MALE
+		);
+
+		ProductEntity product = ProductEntity.create(
+			"테스트상품",
+			15_000L,
+			10L,
+			category,
+			seller
+		);
+
+		OrderProductEntity comparisonOrderProduct =
+			OrderProductEntity.create(
+				2L,
+				15_000L,
+				OrderProductStatus.SHIPPING_COMPLETED,
+				order,
+				product
+			);
+
+		OrderProductEntity subjectOrderProduct =
+			OrderProductEntity.create(
+				2L,
+				15_000L,
+				OrderProductStatus.SHIPPING_COMPLETED,
+				order,
+				product
+			);
+
+		// then
+		assertThat(subjectOrderProduct).isNotNull();
+
+		assertThat(subjectOrderProduct)
+			.usingRecursiveComparison()
+			.isEqualTo(comparisonOrderProduct);
+	}
+
+}
