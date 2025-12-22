746bea77b32def646c8dee667aca72084a378f94
junsang-ee <dkwnstkddk@gmail.com>
2025-12-22 11:23:24 +0900

    feat: 거래내역 엔티티 validation 추가 ValidationUtil 로직 개선(throw method 분리 및 NOT_NEGATIVE 로직 생성) ValidationUtil : ValidationNotNull -> validateRequiredEnum 으로 변경 (only enum 체크)

--- src/main/java/com/kt/domain/entity/BankAccountTransactionEntity.java
+++ src/main/java/com/kt/domain/entity/BankAccountTransactionEntity.java
@@ -4,6 +4,8 @@ import com.kt.constant.bankaccount.BankAccountTransactionPurpose;
 import com.kt.constant.bankaccount.BankAccountTransactionType;
 import com.kt.domain.entity.common.BaseEntity;
 
+import com.kt.util.ValidationUtil;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -70,6 +72,10 @@ public class BankAccountTransactionEntity extends BaseEntity {
 		final BigDecimal balanceSnapshot,
 		final UUID targetId
 	) {
+		ValidationUtil.validateRequiredEnum(transactionType, "거래 타입");
+		ValidationUtil.validateRequiredEnum(transactionPurpose, "거래 목적");
+		ValidationUtil.validatePositive(amount.intValue(), "거래 금액");
+		ValidationUtil.validateNonNegative(balanceSnapshot.intValue(), "거래 잔액");
 		return new BankAccountTransactionEntity(
 			bankAccount,
 			transactionType,
