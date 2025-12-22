8a0a8b430d0b6364726c7c9c23bdee14d6f93389
minseo <114375668+oneokiwa@users.noreply.github.com>
2025-12-23 01:36:20 +0900

    feat: 환불 관련 enum 구현

--- src/main/java/com/kt/constant/OrderProductStatus.java
+++ src/main/java/com/kt/constant/OrderProductStatus.java
@@ -6,16 +6,15 @@ import lombok.RequiredArgsConstructor;
 public enum OrderProductStatus {
 	CREATED("주문생성"),
 	PENDING_APPROVE("승인대기"),
-	SHIPPING_READY("배송대기"),
 	SHIPPING("배송중"),
 	SHIPPING_COMPLETED("배송완료"),
 	PURCHASE_CONFIRMED("구매확정"),
-	CANCELED("주문취소"),
+	CANCELED("주문취소"),        // 배송 전 취소
+	REFUND_COMPLETED("환불완료"),
 	RETURN_WAITING("반품대기"),
 	RETURN_SHIPPING("반품배송중"),
 	RETURN_ARRIVAL("반품도착"),
-	RETURN_CONFIRMED("반품확정"),
-	;
+	RETURN_CONFIRMED("반품확정");
 
 	private final String description;
 }
