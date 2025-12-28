e5dae55070c952d25e7092023d68dcaf179930d1
minseo <114375668+oneokiwa@users.noreply.github.com>
2025-12-28 17:45:01 +0900

    refactor: 코드 라인 수정

--- src/main/java/com/kt/repository/cart/CartItemRepository.java
+++ src/main/java/com/kt/repository/cart/CartItemRepository.java
@@ -1,6 +1,5 @@
 package com.kt.repository.cart;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/kt/service/CartService.java
+++ src/main/java/com/kt/service/CartService.java
@@ -6,8 +6,12 @@ import com.kt.domain.entity.CartEntity;
 
 public interface CartService {
 	CartEntity getCart(UUID userId);
+
 	void addItem(UUID userId, UUID productId, int quantity);
+
 	void changeQuantity(UUID userId, UUID cartItemId, int quantity);
+
 	void removeItem(UUID userId, UUID cartItemId);
+
 	void clear(UUID userId);
 }
