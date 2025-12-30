c4c5daa037ea5b39597f5854daf52e09e52435e5
l-lyun <kimdohyun032@gmail.com>
2025-12-30 12:14:05 +0900

    fix: 병합 후 테스트 실패 해결

--- src/main/java/com/kt/domain/entity/FAQEntity.java
+++ src/main/java/com/kt/domain/entity/FAQEntity.java
@@ -34,7 +34,7 @@ public class FAQEntity extends BaseEntity {
 		this.category = category;
 	}
 
-	public static FAQEntity create(String title, String content, FAQCategory category, String fileId) {
+	public static FAQEntity create(String title, String content, FAQCategory category) {
 		return new FAQEntity(title, content, category);
 	}
 
