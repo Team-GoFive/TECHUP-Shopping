f7de3c292473993de8e23d3d2a17e22b4aca96f6
l-lyun <kimdohyun032@gmail.com>
2025-12-31 17:06:57 +0900

    feat: ChatController 구현

---
+++ src/main/java/com/kt/chat/controller/ChatController.java
@@ -0,0 +1,41 @@
+package com.kt.chat.controller;
+
+import java.util.UUID;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+
+import com.kt.chat.service.ChatMessageService;
+import com.kt.security.DefaultCurrentUser;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+	private final SimpMessagingTemplate messagingTemplate;
+	private final ChatMessageService chatMessageService;
+
+	@MessageMapping("/chat/{sessionId}")
+	public void handleChat(
+		@DestinationVariable String sessionId,
+		@Payload String message,
+		@AuthenticationPrincipal DefaultCurrentUser defaultCurrentUser
+	) {
+		UUID senderId = defaultCurrentUser.getId();
+		String role = defaultCurrentUser.getRole().name();
+
+		chatMessageService.save(sessionId, senderId, role, message);
+
+		messagingTemplate.convertAndSend("/sub/chat/" + sessionId, message);
+	}
+
+}
