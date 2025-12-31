05312d107e1375ff2c0e0f63d08d84aca0bd0eb5
terry <tryterry77@gmail.com>
2025-12-31 14:50:57 +0900

    fix: 재고 도메인 테스트 추가

--- src/main/java/com/kt/domain/entity/InventoryEntity.java
+++ src/main/java/com/kt/domain/entity/InventoryEntity.java
@@ -1,8 +1,11 @@
 package com.kt.domain.entity;
 
 import com.kt.domain.entity.common.BaseEntity;
+import com.kt.util.ValidationUtil;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +16,20 @@ import lombok.NoArgsConstructor;
 public class InventoryEntity extends BaseEntity {
 
 	private Long stock;
+
+	@OneToOne
+	@JoinColumn(name = "product_id", nullable = false)
+	private ProductEntity productEntity;
+
+	protected InventoryEntity(ProductEntity productEntity, Long stock) {
+		this.stock = stock;
+		this.productEntity = productEntity;
+	}
+
+	public static InventoryEntity create(ProductEntity productEntity, Long stock) {
+		ValidationUtil.validatePositive(stock, "재고수량");
+		return new InventoryEntity(productEntity, stock);
+	}
 
 	public void addStock(Long quantity) {
 		this.stock += quantity;
---
+++ src/test/java/com/kt/domain/entity/InventoryEntityTest.java
@@ -0,0 +1,56 @@
+package com.kt.domain.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.kt.common.SellerEntityCreator;
+import com.kt.exception.FieldValidationException;
+
+@ActiveProfiles("test")
+class InventoryEntityTest {
+
+	private ProductEntity testProduct;
+
+	@BeforeEach
+	void setup() {
+		CategoryEntity category = CategoryEntity.create(
+			"테스트카테고리",
+			null
+		);
+
+		SellerEntity seller = SellerEntityCreator.createSeller();
+
+		testProduct = ProductEntity.create(
+			"테스트상품",
+			15_000L,
+			10L,
+			category,
+			seller
+		);
+	}
+
+	@Test
+	void 객체생성_성공() {
+		InventoryEntity inventory = InventoryEntity.create(
+			testProduct,
+			1L
+		);
+
+	}
+
+	@Test
+	void 객체생성_실패__재고값_음수() {
+		assertThrowsExactly(
+			FieldValidationException.class,
+			() -> {
+				InventoryEntity.create(
+					testProduct,
+					-1L
+				);
+			}
+		);
+	}
+}
