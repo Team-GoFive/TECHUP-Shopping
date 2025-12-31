3aa3375d6c99d969ce519c20fea59b339d5bab68
l-lyun <kimdohyun032@gmail.com>
2025-12-31 19:07:36 +0900

    feat: ChatRoomRepository 생성

--- src/main/java/com/kt/chat/repository/chatroom/ChatRoomRepository.java
+++ src/main/java/com/kt/chat/repository/chatroom/ChatRoomRepository.java
@@ -1,10 +1,21 @@
 package com.kt.chat.repository.chatroom;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kt.chat.domain.entity.ChatRoomEntity;
+import com.kt.constant.message.ErrorCode;
+import com.kt.exception.CustomException;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, UUID> {
+
+	Optional<ChatRoomEntity> findBySessionId(UUID sessionId);
+
+	default ChatRoomEntity findBySessionIdOrElseThrow(UUID sessionId) {
+		return findBySessionId(sessionId)
+			.orElseThrow(() -> new CustomException(ErrorCode.SESSION_NOT_FOUND));
+	}
 }
+
