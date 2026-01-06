77287c3df30c6cbbcb87ea3ec446e2f0c86057b0
l-lyun <kimdohyun032@gmail.com>
2026-01-06 15:23:35 +0900

    fix: Override 명시

--- src/main/java/com/kt/chat/service/ChatMessageServiceImpl.java
+++ src/main/java/com/kt/chat/service/ChatMessageServiceImpl.java
@@ -21,11 +21,13 @@ public class ChatMessageServiceImpl implements ChatMessageService {
 
 	private final ChatMessageRepository chatMessageRepository;
 
+	@Override
 	public void save(UUID conversationId, UUID senderId, String senderRole, String message) {
 		ChatMessageEntity msg = ChatMessageEntity.create(conversationId, senderId, senderRole, message);
 		chatMessageRepository.save(msg);
 	}
 
+	@Override
 	public Page<ChatResponse.Search> getMessages(
 		UUID conversationId,
 		Instant cursor,
