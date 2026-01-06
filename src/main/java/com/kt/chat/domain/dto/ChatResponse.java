8bb038e47e958a0c29eabd96c03756f6d7c7fd1b
l-lyun <kimdohyun032@gmail.com>
2026-01-06 14:10:03 +0900

    feat: 채팅 메시지 조회용 리스폰스

--- src/main/java/com/kt/chat/domain/dto/ChatResponse.java
+++ src/main/java/com/kt/chat/domain/dto/ChatResponse.java
@@ -1,6 +1,10 @@
 package com.kt.chat.domain.dto;
 
+import java.time.Instant;
 import java.util.UUID;
+
+import com.kt.chat.domain.entity.ChatMessageEntity;
+import com.querydsl.core.annotations.QueryProjection;
 
 public class ChatResponse {
 	public record Message(
@@ -10,4 +14,29 @@ public class ChatResponse {
 	) {
 
 	}
+
+	public record Search(
+		UUID id,
+		UUID senderId,
+		String senderRole,
+		String message,
+		Instant createdAt
+	) {
+		@QueryProjection
+		public Search {
+
+		}
+
+		public static Search from(ChatMessageEntity chatMessageEntity) {
+			return new Search(
+				chatMessageEntity.getId(),
+				chatMessageEntity.getSenderId(),
+				chatMessageEntity.getSenderRole(),
+				chatMessageEntity.getMessage(),
+				chatMessageEntity.getCreatedAt()
+			);
+		}
+
+	}
+
 }
