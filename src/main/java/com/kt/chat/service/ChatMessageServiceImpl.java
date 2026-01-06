c0ce088b77420caf654ef6a93ac6ef13a0e33d4e
l-lyun <kimdohyun032@gmail.com>
2026-01-06 14:10:39 +0900

    feat: 채팅 메시지 조회 서비스 구현

--- src/main/java/com/kt/chat/service/ChatMessageServiceImpl.java
+++ src/main/java/com/kt/chat/service/ChatMessageServiceImpl.java
@@ -1,11 +1,14 @@
 package com.kt.chat.service;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kt.chat.domain.dto.ChatResponse;
 import com.kt.chat.domain.entity.ChatMessageEntity;
 import com.kt.chat.repository.chatmessage.ChatMessageRepository;
 
@@ -16,10 +19,23 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class ChatMessageServiceImpl implements ChatMessageService {
 
-	private final ChatMessageRepository messageRepository;
+	private final ChatMessageRepository chatMessageRepository;
 
 	public void save(UUID conversationId, UUID senderId, String senderRole, String message) {
 		ChatMessageEntity msg = ChatMessageEntity.create(conversationId, senderId, senderRole, message);
-		messageRepository.save(msg);
+		chatMessageRepository.save(msg);
+	}
+
+	public Page<ChatResponse.Search> getMessages(
+		UUID conversationId,
+		Instant cursor,
+		Pageable pageable
+	) {
+		return chatMessageRepository.findByConversationIdWithCursor(
+			pageable,
+			conversationId,
+			cursor
+		).map(ChatResponse.Search::from);
+
 	}
 }
